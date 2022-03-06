d443d4bca70ca8c62e604bd89244812b54aec082
Will Yazdani <yazd0006@gmail.com>
2022-03-06 11:59:29 -0600

    added export modules

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const Engineer = require('Engineer');
-const Intern = require('Intern');
-const Manager = require('Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
 const newEngineer = [
     {
