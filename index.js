3635f006807adb7da5756d4108178c2727688607
Will Yazdani <yazd0006@gmail.com>
2022-03-06 12:31:09 -0600

    added edmployee questions

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
+// initial question
 const newEmployee = () => {
     return inquirer.prompt([{
         name: 'newEmployee',
@@ -18,9 +19,59 @@ const newEmployee = () => {
             console.log('thank you');
         }
     })
+};
+
+// employee info
+const addEmployee = () => {
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'Enter employee name:',
+        validate: (userInput) => {
+            if (userInput) {
+                return true;
+            } return false;
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter employee email:',
+        validate: (userInput) => {
+            if (userInput) {
+                return true;
+            } return false;
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter employee ID:',
+        validate: (userInput) => {
+            if (userInput) {
+                return true
+            } return false;
+        }
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'Select role:',
+        choices: ['Intern', 'Engineer', 'Manager']
+    }
+])
 }
 
-const addEmployee = () => {}
+newEmployee()
+
+
+
+
+
+
+
+
+
 
 const newEngineer = [
     {
